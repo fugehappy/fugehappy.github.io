a0840b5f5a06139fa4509f7eac9f8eb50ab2de51
fugehappy <fugehappy2009@163.com>
2016-11-09 16:02:28 +0800

    update

---
+++ test/birthday/js/index.js
@@ -0,0 +1,24 @@
+define(function(require, exports, module) {
+    var shake = require('shake');
+	var $ = require('fx');
+	window.onload = function() {
+
+		//create a new instance of shake.js.
+		var myShakeEvent = new Shake({
+			threshold: 15
+		});
+
+		// start listening to device motion
+		myShakeEvent.start();
+
+		// register a shake event
+		window.addEventListener('shake', shakeEventDidOccur, false);
+
+		//shake event callback
+		function shakeEventDidOccur () {
+
+			//put your own code here etc.
+			alert('Shake!');
+		}
+	};
+})
