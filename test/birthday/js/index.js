3375c24eb8e0ad4da0becd5bad6bebb6a0c369c1
fugehappy <fugehappy2009@163.com>
2016-11-10 11:23:52 +0800

    update

--- test/birthday/js/index.js
+++ test/birthday/js/index.js
@@ -2,25 +2,33 @@ define(function(require, exports, module) {
 	var preload = require('preloadImg');
     var shake = require('shake');
 	var $ = require('fx');
+	function triggerShake() {
+		if(!$('.moveTel').hasClass('none')) {
+			//create a new instance of shake.js.
+			var myShakeEvent = new Shake({
+				threshold: 15
+			});
+
+			// start listening to device motion
+			myShakeEvent.start();
+
+			// register a shake event
+			window.addEventListener('shake', shakeEventDidOccur, false);
+		}
+	}
+	
+	function shakeEventDidOccur () {
+
+		//put your own code here etc.
+		//$('.moveTel').addClass('none');
+		var arr=['1','2','3','4'];
+		var i=Math.ceil(Math.random()*5);
+		alert(arr[i]);
+	}	
+	
 	window.onload = function() {
 
-		//create a new instance of shake.js.
-		var myShakeEvent = new Shake({
-			threshold: 15
-		});
-
-		// start listening to device motion
-		myShakeEvent.start();
-
-		// register a shake event
-		window.addEventListener('shake', shakeEventDidOccur, false);
-
-		//shake event callback
-		function shakeEventDidOccur () {
-
-			//put your own code here etc.
-			alert('shake!');
-		}
+		triggerShake();
 	};
 	
 	//图片预加载
@@ -34,5 +42,6 @@ define(function(require, exports, module) {
 				$('.loading').remove()
 			}, 200)
 		}
-	})
+	});
+
 })
