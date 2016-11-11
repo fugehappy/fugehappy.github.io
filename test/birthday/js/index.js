90ef050368e2bc9fe9aa7bdea5c6f62292c2a296
fugehappy <fugehappy2009@163.com>
2016-11-11 14:14:01 +0800

    update

--- test/birthday/js/index.js
+++ test/birthday/js/index.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
 	var preload = require('preloadImg');
-    var shake = require('shake');
+	var shake = require('shake');
 	var $ = require('fx');
 	function triggerShake() {
 		if(!$('.moveTel').hasClass('none')) {
@@ -20,9 +20,10 @@ define(function(require, exports, module) {
 	function shakeEventDidOccur () {
 
 		//put your own code here etc.
-		//$('.moveTel').addClass('none');
+		$('.moveTel').addClass('none');
 		var arr=['1','2','3','4'];
-		var i=Math.ceil(Math.random()*5);
+		var i=Math.floor(Math.random()*5);
+		//
 		alert(arr[i]);
 	}	
 	
@@ -32,7 +33,7 @@ define(function(require, exports, module) {
 	};
 	
 	//图片预加载
-    preload.init({
+	preload.init({
 		imgs: [
 			'./images/jinmao-load.png'
 		],
