55a698321edbb4b311d58241d59df2e35325fd7e
fugehappy <fugehappy2009@163.com>
2016-12-02 10:12:28 +0800

    birth

--- test/birthday/js/index.js
+++ test/birthday/js/index.js
@@ -34,6 +34,17 @@ define(function(require, exports, module) {
 	window.onload = function() {
 		triggerShake();
 	};
+	//判断手机横竖屏状态：
+	window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
+        if (window.orientation === 180 || window.orientation === 0) {
+			alert('h')
+			myShakeEvent.stop();
+        } 
+        if (window.orientation === 90 || window.orientation === -90 ){
+			alert('v')
+			myShakeEvent.start();
+        }  
+    }, false);
 	
 	//图片预加载
 	preload.init({
