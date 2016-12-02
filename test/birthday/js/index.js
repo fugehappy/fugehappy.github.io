9ba76dc5d182a9d74949dc0ad6bfc1f88830eea6
fugehappy <fugehappy2009@163.com>
2016-12-02 10:29:43 +0800

    birth

--- test/birthday/js/index.js
+++ test/birthday/js/index.js
@@ -4,7 +4,7 @@ define(function(require, exports, module) {
 	var $ = require('fx');
 	//create a new instance of shake.js.
 	var myShakeEvent = new Shake({
-		threshold: 15
+		threshold: 10
 	});
 	function triggerShake() {		
 		
