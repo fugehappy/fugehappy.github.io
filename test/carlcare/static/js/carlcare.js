8727c03390acc42002606511dc3e12a240cdef73
henry.luo <henry.luo@silksoftware.com>
2017-09-11 13:09:39 +0800

    carlcare

--- test/carlcare/static/js/carlcare.js
+++ test/carlcare/static/js/carlcare.js
@@ -42,18 +42,53 @@
             $('.carl-product-detail-list').hide().eq(index).fadeIn();
         })
 
-        // 效果
-        $('.sp-effect1').waypoint(function(){
-            // $(this).toggleClass('active');
-            $(this).toggleClass('animation-fade-in-up');
-        }, {
-            offset: '100%'
+        var istrue = true;
+        $(window).on("scroll", function () {
+            // 数字加载动画
+            // countUp();
+        })
+        // countUp();
+        function countUp () {
+            var s = $(window).scrollTop();
+            var t = $('.statistics').offset().top;
+            if (s == t) {
+                $(".timer").countTo({
+                    lastSymbol: "+", //显示在最后的字符
+                    from: 0,  // 开始时的数字
+                    speed: 2000,  // 总时间
+                    refreshInterval: 30,  // 刷新一次的时间
+                    beforeSize: 0, //小数点前最小显示位数，不足的话用0代替
+                    decimals: 0,  // 小数点后的位数，小数做四舍五入
+                    onUpdate: function() {
+                    },  // 更新时回调函数
+                    onComplete: function() {
+                        for(i in arguments){
+                            //console.log(arguments[i]);
+                        }
+                    }
+                });
+            }
+
+        }
+
+        // hover
+        $('.android-zone').hover(function () {
+            $('.android').css({ 'top': '60px'})
+        }, function () {
+            $('.android').css({ 'top': '-200px'})
         });
-        $('.sp-effect2').waypoint(function(){
-            // $(this).toggleClass('active');
-            $(this).toggleClass('animation-fade-in-dowm');
+        $('.google-zone').hover(function () {
+            $('.google-play').css({ 'top': '60px'})
+        }, function () {
+            $('.google-play').css({ 'top': '-200px'})
+        })
+
+        // 效果
+        $('.custom-load').waypoint(function(){
+            $(this).toggleClass('loaded');
         }, {
-            offset: '100%'
+            offset: '50%',
+            triggerOnce: true
         });
     })
 
